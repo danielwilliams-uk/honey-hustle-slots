b8894f013f67186e77b91be5c23887872f171972
Daniel <ds23@hotmail.co.uk>
2020-01-09 01:38:53 +0000

    Splash screen now removes nodes from DOM

--- scripts/game/SplashScreen.js
+++ scripts/game/SplashScreen.js
@@ -5,12 +5,15 @@ define(function () {
         var self = this,
             showSplashScreen = false;
 
-        self.hideSplashScreen = function (obj) {
+        self.hideSplashScreen = function (ele, btn) {
             return new Promise(function (resolve) {
                 if (showSplashScreen) return;
                 showSplashScreen = true;
-                createjs.Tween.get(obj).to({alpha: 0}, 0)
-                    .call(resolve, [showSplashScreen]);
+                /*createjs.Tween.get(obj).to({alpha: 0}, 0)
+                    .call(resolve, [showSplashScreen]);*/
+                ele.parentNode.removeChild(ele);
+                btn.parentNode.removeChild(btn);
+                return resolve(showSplashScreen);
             });
         };
     }
